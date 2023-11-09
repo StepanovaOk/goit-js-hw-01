b10bea317c772f5a4c5ac49bacf0eb0bcc086741
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2023-11-09 22:45:56 +0200

    start1

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+let makeTransaction;
+makeTransaction: quantity;
+makeTransaction: pricePerDroid;
+return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+
+console.log(makeTransaction(5, 3000));
