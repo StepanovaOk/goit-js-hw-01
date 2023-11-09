b2a2930477a674090ff43d94b65b5ba8a0b82c1e
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2023-11-09 23:10:34 +0200

    cannot get console

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
-let makeTransaction;
-makeTransaction: quantity;
-makeTransaction: pricePerDroid;
-return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
 
 console.log(makeTransaction(5, 3000));
